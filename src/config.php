7dc1c3113d55a6cb45ac108f7c60065214d19456
Diogo Lucas de Oliveira <dluks82@gmail.com>
2024-06-28 01:52:41 -0300

    fix: conditional load `autoload.php`

--- src/config.php
+++ src/config.php
@@ -1,12 +1,23 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
 
-use Dotenv\Dotenv;
+// Verificar se estamos em um ambiente local
+$appEnv = getenv('APP_ENV') ?: 'production';
 
-// Carregar variáveis de ambiente do arquivo .env no ambiente de desenvolvimento
-if (file_exists(__DIR__ . '/../.env')) {
-  $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-  $dotenv->load();
+if ($appEnv === 'local') {
+    // Carregar o autoload do Composer se existir
+    if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+        require_once __DIR__ . '/vendor/autoload.php';
+
+        use Dotenv\Dotenv;
+
+        // Carregar variáveis de ambiente do arquivo .env
+        if (file_exists(__DIR__ . '/../.env')) {
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+            $dotenv->load();
+        }
+    } else {
+        die('Erro: Arquivo vendor/autoload.php não encontrado. Execute "composer install".');
+    }
 }
 
 // Acessar variáveis de ambiente
@@ -18,5 +29,5 @@ $database = getenv('MYSQL_DATABASE') ?: 'taskflow';
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
